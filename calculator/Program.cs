f395afa13fdcf3c2f45d74626f8daa8670bce405
Smain Ali <74659909+SmaniAli@users.noreply.github.com>
2023-09-27 10:29:12 +0100

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("please enter first num");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("please enter the operation");
+string operation = Console.ReadLine();
+Console.WriteLine("please enter first num");
+int secondNum = int.Parse(Console.ReadLine());
+
+int result;
+if (operation == "+") result = firstNum + secondNum;
+else if (operation == "-") result = firstNum - secondNum;
+else if (operation == "*") result = secondNum * firstNum;
+else if (operation == "/") result = firstNum / secondNum;
+else result = 0;
+
+Console.WriteLine(result);
